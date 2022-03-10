7f4833caa4f06b458ee7d55ad6f903ce882778e1
Lara Filippone <lara.filippone@gmail.com>
2022-03-10 15:45:08 +0100

    JSNACK2 - created FOR cycle to ask for 6 times a number with prompt

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -5,3 +5,8 @@ console.log('script ok, JNASCK2');
 
 // Creo array vuoto
 let arrayDispari[];
+
+// Creo ciclo for che chieda per 6 vollte un numero all'utente
+for(let i=0; i<5; i++){
+    let numero=prompt('inseriisci un numero');
+}
