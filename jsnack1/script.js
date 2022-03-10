1e91f4299d4e9d5a87cf1ecd0beb26c9d9cfc321
Lara Filippone <lara.filippone@gmail.com>
2022-03-10 15:17:55 +0100

    JSNACK1 - control to check if there is a correct linking between html and js

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,1 +1,4 @@
 /*In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.*/
+
+// Controllo collegamento tra index e script
+console.log('script ok, JNASCK1');
