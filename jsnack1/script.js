14be7d3042b5f12d25a9e6b28718973a77a3a2bc
Lara Filippone <lara.filippone@gmail.com>
2022-03-10 15:36:05 +0100

    JSANCK 1 - added while and for cycles to  verify nomePersona in listaInvitatiFesta

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,3 +2,22 @@
 
 // Controllo collegamento tra index e script
 console.log('script ok, JNASCK1');
+
+// Creo array  contenente lista invitati
+let listaInvitatiFesta = ['Rossi', 'Verdi', 'Neri', 'Blu', 'Bianchi'];
+
+//Tramite prompt chiedo nome all'utente e verifiico se fa parte dellla llista, sennò chiedo di inserirne un altro
+let permesso = false;
+
+
+while(!permesso){
+    let nomePersona = prompt('Inserisci il tuo nome');
+    for (let i=0; i<listaInvitatiFesta.length; i++){
+        if (nomePersona === listaInvitatiFesta[i]){
+            console.log('Evvai!! Sei invitato alla festa del grande Gatsby');
+        }else{
+            console.log('Mi spiace ma non sei invitato!');
+        }
+
+    }
+}
