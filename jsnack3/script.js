98dbc5d2bb7ec42442279a5b147e4d2534e6d54f
Lara Filippone <lara.filippone@gmail.com>
2022-03-10 16:53:01 +0100

    JSNACK3 - create a console.log to see if js file and html file are coorrected linked

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,1 +1,5 @@
 /*Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero. (questo potrebbe richiedere un minimo di ricerca)*/
+
+// Controllo collegamento tra index e script
+console.log('script ok, JNASCK2');
+
