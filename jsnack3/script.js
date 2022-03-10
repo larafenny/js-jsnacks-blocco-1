12514286b906bc40507a752961783cc30716a8bd
Lara Filippone <lara.filippone@gmail.com>
2022-03-10 16:57:46 +0100

    let numero from prompt

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,5 +1,7 @@
 /*Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero. (questo potrebbe richiedere un minimo di ricerca)*/
 
 // Controllo collegamento tra index e script
-console.log('script ok, JNASCK2');
+console.log('script ok, JNASCK3');
 
+// Creo variabile con numero dato dall'utente
+let numero=parseInt(prompt('inserisci un numero di 4 cifre'));
